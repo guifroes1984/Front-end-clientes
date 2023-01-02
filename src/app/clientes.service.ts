e5d90245f83a96363d3520e4b6276c1522eabea8
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-02 16:11:59 -0300

    Salvando clientes atraves da aplicação web para a REST API

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
 import { Cliente } from './clientes/cliente';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,11 @@ import { Cliente } from './clientes/cliente';
 export class ClientesService {
 
   constructor(private http: HttpClient) { 
+
+  }
+
+  salvar(cliente:Cliente) : Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes' , cliente);
   }
 
   getCliente() : Cliente {
