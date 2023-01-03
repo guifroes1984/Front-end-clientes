7a796352233f516f5768bfafe8e7d21b0164529c
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-02 21:48:56 -0300

    Criando componente de listagem de clientes.

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -16,11 +16,19 @@ export class ClientesService {
   salvar(cliente:Cliente) : Observable<Cliente> {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes' , cliente);
   }
+/*
+ getClientes() : Observable<Cliente[]> {
+    return null;
+ }
+ */
 
-  getCliente() : Cliente {
-    let cliente : Cliente = new Cliente();
-    cliente.nome = 'Guilherme';
-    cliente.cpf = '32938645899';
-    return cliente;
-  }
+getClientes() : Cliente[] {
+  let cliente = new Cliente();
+  cliente.id = 1;
+  cliente.nome = 'Guilherme';
+  cliente.dataCadastro = '02/01/2022';
+  cliente.cpf = '32938645899';
+  return [cliente]
+}
+
 }
