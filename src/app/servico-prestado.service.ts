ffa2c12a6d08a96f9e962bf0ac17ba91d4bcc7cd
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-04 15:34:03 -0300

    Implementado a pesquisa de Serviços Prestados.

--- src/app/servico-prestado.service.ts
+++ src/app/servico-prestado.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
 import { environment } from '../environments/environments'
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-lista/servicoPrestadoBusca';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,15 @@ export class ServicoPrestadoService {
   salvar(servicoPrestado: ServicoPrestado) : Observable<ServicoPrestado> {
     return this.http.post<ServicoPrestado>(this.apiURL, servicoPrestado)
   }
+
+  buscar(nome: string, mes: number) : Observable<ServicoPrestadoBusca[]>{
+
+    const httpParams = new HttpParams()
+      .set("nome", nome)
+      .set("mes", mes.toString());
+    
+    const url = this.apiURL + "?" + httpParams.toString();
+    return this.http.get<any>(url);
+  }
+
 }
